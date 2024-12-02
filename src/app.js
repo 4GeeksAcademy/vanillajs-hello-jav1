8b006d8ddba5181f030468f26a1b757f2d5049f9
Nandito54 <nanditojose54@gmail.com>
2024-12-02 17:02:17 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronum = ["the", "our", "a"];
+  let adjective = ["great", "big", "wonderful", "cray"];
+  let noun = ["jogger", "racoon", "cat", "pig"];
+  let ext = [".com", ".net", "es", "outlook", "dev", "uy"];
+
+  for (let i = 0; i < pronum.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let h = 0; h < ext.length; h++) {
+          console.log(pronum[i] + adjective[j] + noun[k] + ext[h]);
+        }
+      }
+    }
+  }
 };
